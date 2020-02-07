065f9dc3b8b3486ced208ab0f0cac81913a2d8eb
geekwise-korey-nichols <geekwise.korey.nichols@gmail.com>
2020-02-07 14:18:21 -0800

    heroku testing

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://node-test-korey.herokuapp.com/api',
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
